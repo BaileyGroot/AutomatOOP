3ecda7be72e158cecef0d83391474bc81dc49ec9
Grootrix <grootrix@gmail.com>
2021-09-14 15:07:37 +0200

    Add project files.

---
+++ AutomatOOP/AdminPanel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace AutomatOOP
+{
+    class AdminPanel
+    {
+        public void Start()
+        {
+            Title = "AdminPanel"!;
+            RunMainMenu();
+        }
+        private void RunMainMenu()
+        {
+            string prompt = @"" + "\n";
+          
+
+            string[] options = { "Add item", "Delete item", "Change item price", "Back"  };
+            Menu mainMenu = new Menu(prompt, options);
+            int selectedIndex = mainMenu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    RunFirstChoice();
+                    break;
+                case 1:
+                    RunSecondChoice();
+                    break;
+                case 2:
+                    RunThirdChoice();
+                    break;
+                case 3:
+                    RunFourChoice();
+                    break;
+            }
+        }
+
+        private void RunFirstChoice()
+        {
+            WriteLine("Add an item");
+            string name = ReadLine();
+            Write("Name: " + name);
+            double price = Convert.ToInt32(ReadLine());
+            Write("Price: " + price);
+
+            var creatingItem = new ProductData(name, price);
+            creatingItem.products.Add(name);
+        }
+        private void RunSecondChoice()
+        {
+            WriteLine("Delete an item");
+            string name = ReadLine();
+            Write("Name: " + name);
+
+            var creatingItem = new ProductData();
+            creatingItem.products.Add();
+        }
+        private void RunThirdChoice()
+        {
+
+        }
+        private void RunFourChoice()
+        {
+
+        }
+
+    }
+}
---
+++ AutomatOOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace AutomatOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Vending Machine");
+
+            WriteLine(" ");
+
+            WriteLine("|  [1] item   |  [2] item |  [3] item |  [4] item |  [5] item |  [6] item | \n" +
+                      "|  [7] item   |  [8] item |  [9] item | [10] item | [11] item | [12] item | \n" +
+                      "| [13] item   | [14] item | [15] item | [16] item | [17] item | [18] item | \n");
+
+            WriteLine(" ");
+
+            WriteLine("Write the number of the product you want to purchase.");
+            string product = ReadLine();
+            Write("Product: " + product);
+
+            ReadKey();
+
+        }
+    }
+}
